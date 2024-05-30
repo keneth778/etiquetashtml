4c839b5d837efb737a33ca683e678e833c2329e2
keneth <keneth.javier90@gmail.com>
2024-05-30 11:42:45 -0600

    hedear_footer

--- footer/footer.js
+++ footer/footer.js
@@ -1,3 +1,16 @@
 let footer_DOM = document.querySelector(".footer");
 
-footer_DOM.innerHTML =`hola soy footer`
+footer_DOM.innerHTML =`
+<div class="box">
+    <img src="https://th.bing.com/th/id/OIP.bAGiEASx0h2dR54gfAtbmAHaDz?rs=1&pid=ImgDetMain" alt="">
+</div>
+<nav class="git">
+    <ul><a href="https://github.com/Leibril2007">ariel</a>
+        <a href="https://github.com/sofiagarcia358">sofia</a>
+        <a href="https://github.com/marianaseven2013">genesis</a>
+        <a href="https://github.com/keneth778">keneth</a>
+    </ul>
+
+</nav>
+    
+`
